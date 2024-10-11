ba226dfd693a0a5ccf56950803d3b8565677a2ba
denismiranda <denismiranda148@gmail.com>
2024-10-10 20:30:10 -0400

    adding content studied today

---
+++ Const/script.js
@@ -0,0 +1,10 @@
+//A constant creates a variable whose value is fixed (cannot be changed)
+
+const number = 42
+console.log(number)
+
+/*
+const number = 55
+console.log(number)
+    A constant cannot have its value changed, because of this line of code will give an error
+*/
